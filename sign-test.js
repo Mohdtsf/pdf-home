e2f4f772d2ceb806df067e89f044b87915b36e77
Mohd Tauseef Ansari <mohdtauseefansari34@gmail.com>
2026-06-06 14:30:11 +0530

    chore: untrack .env.local.example, developer markdown files, and scratch scripts

--- sign-test.js
+++
@@ -1,8 +0,0 @@
-import signpdf from "@signpdf/signpdf";
-import { plainAddPlaceholder } from "@signpdf/placeholder-plain";
-import { P12Signer } from "@signpdf/signer-p12";
-import forge from "node-forge";
-
-console.log("signpdf:", typeof signpdf.sign);
-console.log("plainAddPlaceholder:", typeof plainAddPlaceholder);
-console.log("P12Signer:", typeof P12Signer);
